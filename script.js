0f16ff0afa005460a3aa76fafb1f2156f82d5264
Henry Han <hanchi18@163.com>
2016-02-01 17:21:22 +0800

    commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 ﻿$(document).ready(function() {
 	$("#test").val(window.location.href);
+	$(".scenario p.scenario_head").click(function() {
+                $(this).next("div.scenario_body").slideToggle(300);
+
+    });
 })
